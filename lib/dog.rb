e34681ae35b6071708162c2a2088c4460210af57
Barrett Anderson <barrettanderson1989@gmail.com>
2020-06-11 19:34:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,16 @@
 # Add your code here
 class Dog
   attr_accessor :name
+<<<<<<< HEAD
 
+=======
+>>>>>>> 6cced05412537f574d5fee9e3e1ff9cc4b314b70
   @@all = []
 
 
   def initialize(name)
     @name = name
+<<<<<<< HEAD
     puts save
   end
 
@@ -19,13 +23,36 @@ class Dog
   end
 
   def self.print_all
+=======
+    @@all << self
+  end
+
+  def self.all
+>>>>>>> 6cced05412537f574d5fee9e3e1ff9cc4b314b70
     @@all.each do |dog|
       puts dog.name
     end
   end
 
+<<<<<<< HEAD
   def save
     @@all << self
   end
 
+=======
+  def self.clear_all
+    @@all.clear
+  end
+
+  # def self.print_all
+  #   @@all.each do |dog|
+  #     puts dog.name
+  #   end
+  # end
+  #
+  # def save
+  #
+  # end
+
+>>>>>>> 6cced05412537f574d5fee9e3e1ff9cc4b314b70
 end
